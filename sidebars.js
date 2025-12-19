0fb817cc0412b8e7176671e2615ffd854bd3fd45
Maha Madad Ali <maha.madad@paysyslabs.com>
2025-12-19 11:22:09 +0500

    Finalize OpenRemit documentation and configurations

---
+++ sidebars.js
@@ -0,0 +1,17 @@
+/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
+const sidebars = {
+  mainSidebar: [
+    {
+      type: 'doc',
+      id: 'overview/index',
+      label: 'Overview',
+    },
+    {
+      type: 'doc',
+      id: 'backoffice/index',
+      label: 'Backoffice',
+    },
+  ],
+};
+
+module.exports = sidebars;
